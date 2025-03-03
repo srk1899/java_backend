c90b157b8db8746e6452bf2c50d9450e9ae8bc56
Nishant Sorout <nishant.sorout@bellblazetech.com>
2025-03-03 15:23:53 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Calculator {
+public class calculator {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter first number: ");
